e78fbaa774d6538ccd10f350048e9b303a2d5e6d
Munna-MR7 <munna.cse6.bu@gmail.com>
2024-10-10 01:26:46 +0600

    Generate and download a pdf copy

--- src/main/java/com/project/Recommendation_Based/Telemedicine/controller/AppointmentController.java
+++ src/main/java/com/project/Recommendation_Based/Telemedicine/controller/AppointmentController.java
@@ -8,8 +8,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.itextpdf.text.DocumentException;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+
 
 @Controller
 public class AppointmentController {
@@ -36,4 +48,33 @@ public class AppointmentController {
         //ra.addFlashAttribute("msg", "Congratulations! You made an appointment.");
         return "appointmentSuccess";
     }
+
+
+    // Endpoint to download the PDF of the appointment receipt
+    @GetMapping("/appointment/download/{id}")
+    public ResponseEntity<InputStreamResource> downloadAppointmentReceipt(@PathVariable Integer id) throws DocumentException, IOException {
+        // Fetch appointment details from the service layer
+        Appointment appointment = appointmentService.getAppointmentById(id);
+
+        // Generate the PDF using the service layer
+        ByteArrayInputStream bis = appointmentService.generateAppointmentReceipt(appointment);
+
+        // Set HTTP headers for the response
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=appointment_" + id + ".pdf");
+
+        // Return the PDF file as a ResponseEntity
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bis));
+    }
 }
+
+
+
+
+
+
+
+
